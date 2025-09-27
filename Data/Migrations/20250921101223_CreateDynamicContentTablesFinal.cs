a2d278945d047bbbbb454075f598348f4f1bb0f6
CodeByMoheb <mohebullah.cse@gmail.com>
2025-09-27 22:59:49 +0600

    AddDynamicContentTablesFixed

---
+++ Data/Migrations/20250921101223_CreateDynamicContentTablesFinal.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sector_13_Welfare_Society___Digital_Management_System.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateDynamicContentTablesFinal : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "dbo");
+
+            // Check if tables exist in zidan schema before renaming
+            migrationBuilder.Sql(@"
+                IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'zidan' AND TABLE_NAME = 'PreviousCandidates')
+                BEGIN
+                    ALTER SCHEMA [dbo] TRANSFER [zidan].[PreviousCandidates];
+                END
+            ");
+
+            migrationBuilder.Sql(@"
+                IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'zidan' AND TABLE_NAME = 'LeadershipMessages')
+                BEGIN
+                    ALTER SCHEMA [dbo] TRANSFER [zidan].[LeadershipMessages];
+                END
+            ");
+
+            migrationBuilder.Sql(@"
+                IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'zidan' AND TABLE_NAME = 'ElectedCandidates')
+                BEGIN
+                    ALTER SCHEMA [dbo] TRANSFER [zidan].[ElectedCandidates];
+                END
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // Ensure zidan schema exists
+            migrationBuilder.EnsureSchema(
+                name: "zidan");
+
+            // Check if tables exist in dbo schema before renaming back
+            migrationBuilder.Sql(@"
+                IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'PreviousCandidates')
+                BEGIN
+                    ALTER SCHEMA [zidan] TRANSFER [dbo].[PreviousCandidates];
+                END
+            ");
+
+            migrationBuilder.Sql(@"
+                IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'LeadershipMessages')
+                BEGIN
+                    ALTER SCHEMA [zidan] TRANSFER [dbo].[LeadershipMessages];
+                END
+            ");
+
+            migrationBuilder.Sql(@"
+                IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'dbo' AND TABLE_NAME = 'ElectedCandidates')
+                BEGIN
+                    ALTER SCHEMA [zidan] TRANSFER [dbo].[ElectedCandidates];
+                END
+            ");
+        }
+    }
+}
